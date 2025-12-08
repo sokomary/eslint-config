70e9a04caed7a65ca879441c12f141045f624082
sokomary <miss.sokolova-marya2014yandex.ru>
2025-12-08 22:42:28 +0100

    upgrade versions

--- index.js
+++ index.js
@@ -1,63 +1,72 @@
-module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: [
-        'plugin:react/recommended',
-        'airbnb',
-        'airbnb-typescript',
-        'airbnb/hooks',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
+const { defineConfig } = require('eslint/config');
+const react = require('eslint-plugin-react');
+const prettier = require('eslint-config-prettier');
+
+module.exports = defineConfig([
+    {
+        files: ['**/*.{js,jsx,ts,tsx}'],
+
+        languageOptions: {
+            parser: require('@typescript-eslint/parser'),
+            ecmaVersion: 13,
+            sourceType: 'module',
         },
-        ecmaVersion: 13,
-        sourceType: 'module',
-        project: 'tsconfig.json',
-    },
-    plugins: ['react', '@typescript-eslint'],
-    rules: {
-        '@typescript-eslint/member-delimiter-style': 'error',
-        '@typescript-eslint/no-use-before-define': 'off',
-        '@typescript-eslint/type-annotation-spacing': ['error', {
-            before: false,
-            after: true,
-            overrides: {
-                arrow: {
-                    before: true,
-                    after: true,
+
+        plugins: {
+            react,
+            prettier,
+        },
+
+        rules: {
+            'react/button-has-type': 'off',
+            'no-plusplus': 'off',
+            'no-nested-ternary': 'off',
+            'react/jsx-key': 'error',
+            'brace-style': [
+                'error',
+                '1tbs',
+                {
+                    allowSingleLine: false,
                 },
-            },
-        }],
-        'brace-style': ['error', '1tbs', {
-            allowSingleLine: false,
-        }],
-        curly: ['error', 'all'],
-        'import/no-extraneous-dependencies': ['error', {
-            devDependencies: ['**/stories/**'],
-        }],
-        'import/prefer-default-export': 'off',
-        'max-len': ['error', {
-            code: 120,
-            ignoreTemplateLiterals: true,
-        }],
-        'no-underscore-dangle': ['error', {
-            allow: ['_env_'],
-        }],
-        'react/destructuring-assignment': 'off',
-        'react/function-component-definition': ['error', {
-            'namedComponents': 'arrow-function',
-            'unnamedComponents': 'arrow-function',
-        }],
-        'react/jsx-no-useless-fragment': ['error', {
-            'allowExpressions': true,
-        }],
-        'react/jsx-props-no-spreading': 'off',
-        'react/no-array-index-key': 'off',
-        'react/prop-types': 'off',
-        'react/require-default-props': 'off',
+            ],
+
+            curly: ['error', 'all'],
+            'max-len': [
+                'error',
+                {
+                    code: 120,
+                    ignoreTemplateLiterals: true,
+                },
+            ],
+
+            'no-underscore-dangle': [
+                'error',
+                {
+                    allow: ['_env_'],
+                },
+            ],
+
+            'react/destructuring-assignment': 'off',
+
+            'react/function-component-definition': [
+                'error',
+                {
+                    namedComponents: 'arrow-function',
+                    unnamedComponents: 'arrow-function',
+                },
+            ],
+
+            'react/jsx-no-useless-fragment': [
+                'error',
+                {
+                    allowExpressions: true,
+                },
+            ],
+
+            'react/jsx-props-no-spreading': 'off',
+            'react/no-array-index-key': 'off',
+            'react/prop-types': 'off',
+            'react/require-default-props': 'off',
+        },
     },
-};
+]);
